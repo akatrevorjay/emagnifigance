eb94b41a46c353838a46620939e533211f24c213
Trevor Joynson (trevorj) <trevorj@skywww.net>
2013-03-19 02:20:20 -0400

    Cleanup

--- emag/emails/urls.py
+++ emag/emails/urls.py
@@ -1,18 +1,9 @@
 from django.conf.urls import patterns, include, url
 #from django.conf import settings
 
-#from emag.emails.api import EmailCampaignResource
-from .api import EmailCampaignResource
-
-email_campaign_resource = EmailCampaignResource()
-
-
 urlpatterns = patterns(
     'emails.views',
     #url(r'^$', 'test', name='emails'),
-
-    # django-tastypie api
-    url(r'^api/', include(email_campaign_resource.urls)),
 )
 
 #urlpatterns += patterns('',
